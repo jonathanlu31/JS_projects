15af7c0932b076ede7359cf70bbedf8d7a18b978
Jonathan Lu <jonathan.lu31@gmail.com>
2021-06-03 23:11:25 -0700

    Started toast alert

--- JS_miniprojects/script.js
+++ JS_miniprojects/script.js
@@ -5,3 +5,15 @@ function openNav() {
 }
 
 hamburger.addEventListener('click', openNav);
+
+let toaster = document.getElementById('toasts');
+toasts.addEventListener('click', () => {
+    let toast = document.createElement('div');
+    toast.className = 'alert alert-primary';
+    toast.textContent = 'a toast for a start to a new project series!';
+    toast.style.position = 'fixed';
+    document.body.append(toast);
+    setInterval(() => {
+        toast.remove();
+    }, 2000);
+})
