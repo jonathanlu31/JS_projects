b39960efb87d1139eb31c89f4d776641f5f87b90
Jonathan Lu <jonathan.lu31@gmail.com>
2021-06-10 21:14:21 -0700

    Initial quiz app styling, added questions, loop through questions, count correct

---
+++ quiz_app/script.js
@@ -0,0 +1,107 @@
+const quizData = [
+    {
+        question: 'How many undergraduates are enrolled at UC Berkeley',
+        answers: {
+            0: '24,000',
+            1: '51,000',
+            2: '32,000',
+            3: '42,000'
+        },
+        correct: 2
+    },
+    {
+        question: 'Who was not one of the Paypal Mafia?',
+        answers: {
+            0: 'Marc Andreessen',
+            1: 'Elon Musk',
+            2: 'Peter Thiel',
+            3: 'Reid Hoffman'
+        },
+        correct: 0
+    },
+    {
+        question: 'Which early browser is credited to have popularized the World Wide Web?',
+        answers: {
+            0: 'Netscape',
+            1: 'Internet Explorer',
+            2: 'Google Chrome',
+            3: 'Mosaic'
+        },
+        correct: 2
+    },
+    {
+        question: 'What year was UC Berkeley founded?',
+        answers: {
+            0: '1854',
+            1: '1794',
+            2: '1868',
+            3: '1848'
+        },
+        correct: 3
+    },
+    {
+        question: 'Which coding language came first?',
+        answers: {
+            0: 'C++',
+            1: 'JavaScript',
+            2: 'Python',
+            3: 'Ruby'
+        },
+        correct: 0
+    }
+]
+let questionIndex = 0;
+let answersCorrect = 0;
+
+const quiz = document.forms[0];
+quiz.addEventListener('submit', processSubmission);
+displayNextQuestion();
+
+function processSubmission(e) {
+    e.preventDefault();
+    const userAnswer = +collectAnswer();
+    const correctAnswer = quizData[questionIndex].correct;
+    console.log(userAnswer);
+    console.log(quizData[questionIndex].correct)
+    if (userAnswer === correctAnswer) {
+        countAnswer(true);
+    } else {
+        countAnswer(false);
+    }
+    questionIndex++;
+    displayNextQuestion();
+}
+
+function collectAnswer() {
+    let answerChoices = document.querySelectorAll('input[name="quiz-answer"]');
+    for (let choice of answerChoices) {
+        if (choice.checked) {
+            return choice.dataset.answer;
+        }
+    }
+}
+
+function countAnswer(correct) {
+    const alert = document.createElement('div');
+    alert.className = 'alert';
+    alert.role = 'alert';
+    if (correct) {
+        alert.textContent = 'Correct!';
+        alert.classList.add('alert-success');
+        answersCorrect++;
+    } else {
+        alert.textContent = 'Incorrect.';
+        alert.classList.add('alert-danger');
+    }
+
+    document.body.append(alert);
+    setTimeout(() => alert.remove(), 1500);
+}
+
+function displayNextQuestion() {
+    document.querySelector('.card-title').textContent = quizData[questionIndex].question;
+    let answers = document.querySelectorAll('.quiz-answer-container label');
+    for (let i = 0; i < answers.length; i++) {
+        answers[i].textContent = quizData[questionIndex].answers[i];
+    }
+}
