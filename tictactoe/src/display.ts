f7baf760ec22cc50c69aa0c6dd57c4645fa8684c
Jonathan Lu <jonathan.lu31@gmail.com>
2021-07-01 20:07:40 -0700

    Setup eslint, cleaned up code

--- tictactoe/src/display.ts
+++ tictactoe/src/display.ts
@@ -1,74 +1,74 @@
-import { GameBoard } from "./gameboard";
-import { Player } from "./player";
+/* eslint-disable import/no-unresolved */
+import GameBoard from './gameboard';
+import Player from './player';
 
 const turnHeader = document.getElementById('turn')! as HTMLParagraphElement;
-const table = document.querySelector('table')!;
 
-export class DisplayController {
-    private game: GameBoard;
-    private playable: boolean;
+export default class DisplayController {
+  private game: GameBoard;
+  private playable: boolean;
 
-    constructor() {
-        this.game = new GameBoard();
-        this.game.display = this;
-        this.playable = true;
+  constructor() {
+    this.game = new GameBoard();
+    this.game.display = this;
+    this.playable = true;
+  }
+
+  markTile(e: MouseEvent) {
+    if ((e.target! as HTMLElement).tagName !== 'TD') {
+      return;
+    }
+    if (!this.playable) {
+      return;
     }
 
-    markTile(e: MouseEvent) {
-        if ((e.target! as HTMLElement).tagName !== 'TD') {
-            return;
-        }
-        if (!this.playable) {
-            return;
-        }
-
-        let marked = this.game.getCurrentPlayer().mark(e.target as HTMLTableCellElement);
-        if (marked) {
-            this.game.getSnapshot();
-            this.checkGameover();
-            if (this.playable) {
-                this.game.nextTurn();
-            }
-        }
+    const marked = this.game.getCurrentPlayer().mark(e.target as HTMLTableCellElement);
+    if (marked) {
+      this.game.getSnapshot();
+      this.checkGameover();
+      if (this.playable) {
+        this.game.nextTurn();
+      }
     }
+  }
 
-    static changeTurn(currPlayer: Player) {
-        turnHeader.textContent = `Player ${currPlayer.symbol}'s turn`;
-    }
+  static changeTurn(currPlayer: Player) {
+    turnHeader.textContent = `Player ${currPlayer.symbol}'s turn`;
+  }
 
-    checkGameover() {
-        if (this.game.checkWin()) {
-            this.endGame(true);
-            this.playable = false;
-        } else if (this.game.checkTie()) {
-            this.endGame(false);
-        }
+  checkGameover() {
+    if (this.game.checkWin()) {
+      this.endGame(true);
+      this.playable = false;
+    } else if (this.game.checkTie()) {
+      this.endGame(false);
     }
+  }
 
-    reset() {
-        this.game.clearBoard();
-        this.playable = true;
-        turnHeader.textContent = '';
-        this.startGame();
-    }
+  reset() {
+    GameBoard.clearBoard();
+    this.playable = true;
+    turnHeader.textContent = '';
+    this.startGame();
+  }
 
-    endGame(win: boolean) {
-        if (win) {
-            turnHeader.textContent = `Player ${this.game.getCurrentPlayer().symbol} wins!`;
-        } else {
-            turnHeader.textContent = "It's a tie!";
-        }
+  endGame(win: boolean) {
+    if (win) {
+      turnHeader.textContent = `Player ${this.game.getCurrentPlayer().symbol} wins!`;
+    } else {
+      turnHeader.textContent = "It's a tie!";
     }
+  }
 
-    setGameMode(mode: string) {
-        this.game.setMode(mode);
-    }
+  setGameMode(mode: string) {
+    this.game.setMode(mode);
+  }
 
-    startGame() {
-        if (this.game.getMode() === 'single') {
-            this.game.nextTurn();
-            return;
-        }
-        this.game.currentPlayer = 0;
+  startGame() {
+    if (this.game.getMode() === 'single') {
+      this.game.nextTurn();
+      return;
     }
+    this.game.currentPlayer = 0;
+  }
 }
--- tictactoe/src/player.ts
+++ tictactoe/src/player.ts
@@ -1,11 +1,13 @@
-export class Player {
-    constructor(public symbol: string) {}
+export default class Player {
+  // eslint-disable-next-line
+  constructor(public symbol: string) {}
 
-    mark(this: Player, tile: HTMLTableCellElement) {
-        if (tile.textContent) {
-            return false;
-        }
-        tile.textContent = this.symbol;
-        return true;
+  mark(this: Player, tile: HTMLTableCellElement) {
+    if (tile.textContent) {
+      return false;
     }
+    // eslint-disable-next-line no-param-reassign
+    tile.textContent = this.symbol;
+    return true;
+  }
 }
